38f6d13969be2abb2bf1c5f5340a6c52f5943827
james049240 <james049240@gmail.com>
2016-01-09 17:23:10 +0800

    added anna ui

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def render_price( price )
     number_to_currency( price , unit: "", precision: 2, delimiter: ",")
   end
+
+  def redner_col_md_4( title , price )
+    content_tag :div ,  "#{title} #{render_price(price)}" , :class => "col-md-4"
+  end
 end
